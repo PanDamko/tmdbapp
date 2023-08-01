69e6b613d645932523154b914412d8027ba2640e
Kamil Warmuszok <k.warmuszok@alfaelektro.pl>
2023-08-01 17:58:18 +0200

    Test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@
 import './App.css';
 
 function App() {
+
+
+  const getCountries = async () => {
+    const url = query ? `https://restcountries.com/v3.1/name/${query}` : 'https://restcountries.com/v3.1/all'
+    try {
+      
+      const response = await fetch(url);
+      if(response.ok) {
+        const data = await response.json()
+        setCountries(data)
+      } else {
+        setError("Nie udało się pobrać danych")
+      }
+      
+    } catch (error) {
+      console.log("Nie udało się pobrać danych")
+    }
+
+  }
+
   return (
     <div className="App">
       <h1> sadsa</h1>
