4ac175546954c4c7c83484b3ff6e257875e007c4
PanDamko <knapdam@gmail.com>
2023-08-01 23:23:19 +0200

    All v1 gray with search and ligt/dark mode

--- src/components/ChNavBar.js
+++ src/components/ChNavBar.js
@@ -1,26 +1,55 @@
+'use client'
 
-import {  Box,  Flex, Input,  Avatar,HStack,Text,IconButton,Button,Menu,MenuButton,MenuList,MenuItem,MenuDivider,useDisclosure,useColorMode,Stack,Image,} from '@chakra-ui/react'
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  Text,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+} from '@chakra-ui/react'
 import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
- import { MoonIcon, SunIcon } from '@chakra-ui/icons'
-import { Link } from 'react-router-dom'
-import Logo from "../images/free_logos_dribbble_ph.jpg"
 
-const Links = [
-  {name:'Home', slug:'/'}, 
-  {name:'Movie', slug:'/moviepage'},
-  {name:'Actor', slug:'/actorpage'},
-  {name:'Faq', slug:'/faqpage'},
-  {name:'Rules', slug:'/rulespage'}]
-  
+interface Props {
+  children: React.ReactNode
+}
 
+const Links = ['Dashboard', 'Projects', 'Team']
 
+const NavLink = (props: Props) => {
+  const { children } = props
+
+  return (
+    <Box
+      as="a"
+      px={2}
+      py={1}
+      rounded={'md'}
+      _hover={{
+        textDecoration: 'none',
+        bg: useColorModeValue('gray.200', 'gray.700'),
+      }}
+      href={'#'}>
+      {children}
+    </Box>
+  )
+}
 
 export default function Simple() {
   const { isOpen, onOpen, onClose } = useDisclosure()
- const { colorMode, toggleColorMode } = useColorMode()
+
   return (
     <>
-      <Box  px={4}>
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
           <IconButton
             size={'md'}
@@ -30,21 +59,13 @@ export default function Simple() {
             onClick={isOpen ? onClose : onOpen}
           />
           <HStack spacing={8} alignItems={'center'}>
-            <Box><Image boxSize='60px' src={Logo} alt="" /> </Box>
+            <Box>Logo</Box>
             <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
               {Links.map((link) => (
-                <Link to={link.slug} key={link.slug}><Text color="black" > {link.name}</Text></Link>
+                <NavLink key={link}>{link}</NavLink>
               ))}
             </HStack>
           </HStack>
-          
-          
-              <Button onClick={toggleColorMode}>
-                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
-              </Button>
-            <Box><Input size='xs' placeholder='Szukaj' /></Box>
-
-
           <Flex alignItems={'center'}>
             <Menu>
               <MenuButton
@@ -74,14 +95,14 @@ export default function Simple() {
           <Box pb={4} display={{ md: 'none' }}>
             <Stack as={'nav'} spacing={4}>
               {Links.map((link) => (
-                <Link to={link.slug} key={link.slug}>{link.name}</Link>
+                <NavLink key={link}>{link}</NavLink>
               ))}
             </Stack>
           </Box>
         ) : null}
       </Box>
 
-      
+      <Box p={4}>Main Content Here</Box>
     </>
   )
 }
--- src/templetaes/MainTemplates.js
+++ src/templetaes/MainTemplates.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
-import {  Box,} from '@chakra-ui/react'
+import {  Box} from '@chakra-ui/react'
 import { ChakraProvider } from '@chakra-ui/react'
 const MainTemplates = ({children}) => {
   return (
     <div>
       <ChakraProvider>
       <Box  w='100%'   h='100%'    bgGradient={[
-            'linear(to-tr, teal.300, yellow.400)',
-    'linear(to-t, blue.200, teal.500)',
-    'linear(to-b, orange.100, purple.300)',
+            'linear(to-tr, gray.900, gray.300)',
+    'linear(to-t, gray.900, gray.300)',
+    'linear(to-b, gray.900, gray.300)',
   ]}>
       <NavBar />
       
