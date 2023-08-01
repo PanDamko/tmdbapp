c95a7266809149851fc0294e4811f8fc612e9f89
Mateusz Grabiec <mgrabiec9410@gmail.com>
2023-08-01 20:11:05 +0200

    dodana strona faq

--- src/pages/FaqPage.js
+++ src/pages/FaqPage.js
@@ -1,14 +1,71 @@
-import React from 'react'
-import MainTemplates from '../templetaes/MainTemplates'
+import React from 'react';
+import { Box, Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon } from '@chakra-ui/react';
+
 const FaqPage = () => {
   return (
-    <div>
-    <MainTemplates>
+    <Box py="20" maxW="800px" mx="auto">
+      <Accordion allowToggle>
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box flex="1" textAlign="left">
+                Czy dostęp do serwisu wymaga rejestracji?
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb="4">
+            Tak, nasz serwis jest dostępny tylko dla zarejestrowanych użytkowników.
+          </AccordionPanel>
+        </AccordionItem>
 
-     <h1> FaqPage</h1>
-    </MainTemplates>
-  </div>
-  )
-}
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box flex="1" textAlign="left">
+                Jakie są formy płatności za filmy?
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb="4">
+            Obecnie akceptujemy płatności za filmy wyłącznie kryptowalutami oraz BLIKiem.
+          </AccordionPanel>
+        </AccordionItem>
 
-export default FaqPage
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box flex="1" textAlign="left">
+                  Dostaję maile/powiadomienia o dyskusjach na forach, w których biorę udział. Jak mogę z nich zrezygnować?
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb="4">
+          W swoim profilu wejdź w zakładkę "Obserwuję", a potem wciśnij przycisk "Ustawienia obserwowanych". 
+          Na tej stronie będziesz mógł dokładnie wybrać, jakie powiadomienia i jaką drogą chcesz od nas otrzymywać.
+          </AccordionPanel>
+        </AccordionItem>
+
+         <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box flex="1" textAlign="left">
+                  Dostaję maile/powiadomienia o dyskusjach na forach, w których biorę udział. Jak mogę z nich zrezygnować?
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb="4">
+          W swoim profilu wejdź w zakładkę "Obserwuję", a potem wciśnij przycisk "Ustawienia obserwowanych". 
+          Na tej stronie będziesz mógł dokładnie wybrać, jakie powiadomienia i jaką drogą chcesz od nas otrzymywać.
+          </AccordionPanel>
+        </AccordionItem>
+        {/* Dodaj więcej pytań i odpowiedzi jako kolejne elementy AccordionItem */}
+      </Accordion>
+    </Box>
+  );
+};
+
+export default FaqPage;
