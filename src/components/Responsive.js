45d4e793f8da1158a500f7b443e026699e3d4571
PanDamko <knapdam@gmail.com>
2023-08-02 17:31:10 +0200

    Home Page done?

--- src/components/Responsive.js
+++ src/components/Responsive.js
@@ -2,21 +2,26 @@ import React, {useState} from 'react'
 import Slider from "react-slick";
 import {Box, Image, Text} from '@chakra-ui/react'
 const Responsive = ({item}) => {
-    const[settings,setSettings]=useState({dots: true,
-        infinite: false,
-        speed: 2200,
-        slidesToShow: 7,
-        slidesToScroll: 3,
-        initialSlide: 0,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        pauseOnHover: true,
+    const[settings,setSettings]=useState({
+        dots: false,
+        infinite: true,
+        speed: 200,
+        slidesToShow: 6,
+        slidesToScroll: 5,
+        initialSlide: 3,
+        // className: "center",
+        // centerMode: true,
+        // centerPadding: "60px",
+        
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+        // pauseOnHover: true,
         responsive: [
           {
             breakpoint: 1024,
             settings: {
-              slidesToShow: 6,
-              slidesToScroll: 3,
+              slidesToShow: 5,
+              slidesToScroll: 4,
               infinite: true,
               dots: true
             }
@@ -25,7 +30,7 @@ const Responsive = ({item}) => {
             breakpoint: 600,
             settings: {
               slidesToShow: 4,
-              slidesToScroll: 2,
+              slidesToScroll: 3,
               initialSlide: 0
             }
           },
@@ -33,7 +38,7 @@ const Responsive = ({item}) => {
             breakpoint: 480,
             settings: {
               slidesToShow: 3,
-              slidesToScroll: 3
+              slidesToScroll: 2
             }
           }
         ]})
@@ -43,7 +48,7 @@ const Responsive = ({item}) => {
         
    <Slider {...settings}>
 
-          {item.data.results.map((x)=><Box><Image key={x.id}src={`https://image.tmdb.org/t/p/original/${x.poster_path}`} alt=''/>
+          {item.data.results.map((x)=><Box key={x.id}><Image onDoubleClick={()=>console.log(x.id)}  src={`https://image.tmdb.org/t/p/original/${x.poster_path}`} alt=''/>
           {/* <Text>{x.title}</Text>  */}
           </Box>  )}
         </Slider>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,33 +1,38 @@
-import React  from 'react'
+import React from 'react'
 import MainTemplates from '../templetaes/MainTemplates'
 
-import { Input, Text, Box, Center} from '@chakra-ui/react'
+import { Input, Text, Box, Center } from '@chakra-ui/react'
 import Responsive from '../components/Responsive'
 import { useFetch } from '../components/UseFetch'
 
 const HomePage = () => {
-  
+
   const popularFilms = useFetch('https://api.themoviedb.org/3/movie/popular');
-  const popularSerials = useFetch('https://api.themoviedb.org/3/tv/top_rated')
-  const trending = useFetch('https://api.themoviedb.org/3/trending/movie/day')
+  const popularSerials = useFetch('https://api.themoviedb.org/3/tv/popular');
+  const topratedfilms = useFetch('https://api.themoviedb.org/3/movie/top_rated')
+  const topratedSerials = useFetch('https://api.themoviedb.org/3/tv/top_rated')
+  const trending = useFetch('https://api.themoviedb.org/3/trending/all/day')
+  
   return (
     <div>
       <MainTemplates>
         <Center>
-      <Box borderRadius="5%" background='url(https://i.pravatar.cc/300) center/cover no-repeat' w="80" >
-        <Box display='flex' alignItems='center' flexDirection="column"><Text>Witamy na naszej stronie.</Text>
-        <Input w="50%" size='md' placeholder='Szukaj' /></Box>
-        </Box></Center>
-       <h1> Popularne filmy</h1>
-       <Box color='gray.50'  >{popularFilms.loading ? <h1> Loading...</h1>: <Responsive item={popularFilms}/>}</Box>
-       <h1> Popularne seriale</h1>
-        <Box color='gray.50'  >{popularSerials.loading ? <h1> Loading...</h1>: <Responsive item={popularSerials}/>}</Box>
-       <h1> Trending</h1>
-       <Box color='gray.50'  >{trending.loading ? <h1> Loading...</h1>: <Responsive item={trending}/>}</Box>
-     {/*  <h1>popularne</h1>
-       <Box color='gray.50'  ><Responsive films={films}/></Box>
-       <h1>obejrzyj za darmo</h1>
-       <Box color='gray.50'  ><Responsive films={films}/></Box> */}
+          <Box borderRadius="5%" background='url(https://i.pravatar.cc/300) center/cover no-repeat' w="80" >
+            <Box display='flex' alignItems='center' flexDirection="column"><Text>Witamy na naszej stronie.</Text>
+              <Input w="50%" size='md' placeholder='Szukaj' /></Box>
+          </Box></Center>
+        <Text>Popularne filmy</Text>
+        <Box color='gray.50'  >{popularFilms.loading ? <Text> Loading...</Text> : <Responsive item={popularFilms} />}</Box>
+        <Text>Popularne seriale</Text>
+        <Box color='gray.50'  >{popularSerials.loading ? <Text> Loading...</Text> : <Responsive item={popularSerials} />}</Box>
+        
+        <Text>Najlepiej oceniane filmy</Text>
+        <Box color='gray.50'  >{topratedfilms.loading ? <Text> Loading...</Text> : <Responsive item={topratedfilms} />}</Box>
+        <Text>Najlepiej oceniane seriale</Text>
+        <Box color='gray.50'  >{topratedSerials.loading ? <Text> Loading...</Text> : <Responsive item={topratedSerials} />}</Box>
+        
+        <Text>Trendy</Text>
+        <Box color='gray.50'  >{trending.loading ? <Text> Loading...</Text> : <Responsive item={trending} />}</Box>
       </MainTemplates>
     </div>
   )
